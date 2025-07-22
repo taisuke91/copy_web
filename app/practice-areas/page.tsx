20d8289bcd9a5eec86e2e4d754244c6cef3d78dc
taisuke91 <itoutaisuke20030430@gmail.com>
2025-07-22 15:34:03 +0900

    buttonをimport

--- app/practice-areas/page.tsx
+++ app/practice-areas/page.tsx
@@ -1,4 +1,5 @@
-import { Card, CardContent, CardHeader, CardTitle } from "@/components/ui/card"
+import { Button } from "@/components/ui/button"
+import { Card, CardContent, CardHeader, CardTitle} from "@/components/ui/card"
 import { Briefcase, Building, Gavel, HeartHandshake, Home, Scale } from "lucide-react"
 import Link from "next/link"
 
